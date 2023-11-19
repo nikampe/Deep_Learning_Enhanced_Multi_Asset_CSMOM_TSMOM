b16b7b0131c44deba55b746bae76beeaf19a8d12
Niklas Kampe <67834846+nikampe@users.noreply.github.com>
2023-11-19 21:00:43 +0100

    FINAL
    FINAL

--- settings/strategy_params.py
+++ settings/strategy_params.py
@@ -7,25 +7,14 @@ strategy_params_csmom = {
 feature_params_csmom = {
     "cumulative_periods": [
         1, 5, 21, 63, 126, 252],
-        # 21, 63, 126, 252],
-        # int(3*strategy_params_csmom["rebalance_freq"]),
-        # int(6*strategy_params_csmom["rebalance_freq"]),
-        # int(12*strategy_params_csmom["rebalance_freq"])], # 1,21,63,126,252, # [3*strategy_params_csmom["rebalance_freq"], 6*strategy_params_csmom["rebalance_freq"], 12*strategy_params_csmom["rebalance_freq"]]
     "normalized_periods": [
         1, 5, 21, 63, 126, 252],
-        # 21, 63, 126, 252],
-        # int(3*strategy_params_csmom["rebalance_freq"]),
-        # int(6*strategy_params_csmom["rebalance_freq"]),
-        # int(12*strategy_params_csmom["rebalance_freq"])], # [1,21,63,126,252], # [3*strategy_params_csmom["rebalance_freq"], 6*strategy_params_csmom["rebalance_freq"], 12*strategy_params_csmom["rebalance_freq"]]
     "macd_span_short": [8, 16, 32],
     "macd_span_long": [24, 48, 96],
     "macd_std_short": 63,
     "macd_std_long": 252,
     "macd_cumulative_periods": [
         21, 63, 126, 252]}
-        # int(3*strategy_params_csmom["rebalance_freq"]),
-        # int(6*strategy_params_csmom["rebalance_freq"]),
-        # int(12*strategy_params_csmom["rebalance_freq"])]} # [1,3,6,12], # [1*strategy_params_csmom["rebalance_freq"], 3*strategy_params_csmom["rebalance_freq"], 6*strategy_params_csmom["rebalance_freq"], 12*strategy_params_csmom["rebalance_freq"]]
 
 strategy_params_tsmom = {
     "train_interval": 5,
@@ -36,14 +25,8 @@ feature_params_tsmom = {
     "lookback": int(252/strategy_params_tsmom["rebalance_freq"]),
     "cumulative_periods": [
         1, 5, 21, 63, 126, 252],
-        # int(3*strategy_params_tsmom["rebalance_freq"]),
-        # int(6*strategy_params_tsmom["rebalance_freq"]),
-        # int(12*strategy_params_tsmom["rebalance_freq"])], # 1,21,63,126,252
     "normalized_periods": [
         1, 5, 21, 63, 126, 252],
-        # int(3*strategy_params_tsmom["rebalance_freq"]),
-        # int(6*strategy_params_tsmom["rebalance_freq"]),
-        # int(12*strategy_params_tsmom["rebalance_freq"])], # 1,21,63,126,252
     "macd_span_short": [8,16,32],
     "macd_span_long": [24,48,96],
     "macd_std_short": 63,
